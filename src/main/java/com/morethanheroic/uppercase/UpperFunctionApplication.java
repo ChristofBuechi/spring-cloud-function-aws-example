51e91e79abfe41d1e5e0ccb0bf6896865af9c561
Christof Buechi <christof.buechi@gmail.com>
2018-09-02 15:11:53 +0200

    adding database access

--- src/main/java/com/morethanheroic/uppercase/UpperFunctionApplication.java
+++ src/main/java/com/morethanheroic/uppercase/UpperFunctionApplication.java
@@ -1,10 +1,17 @@
 package com.morethanheroic.uppercase;
 
 import com.morethanheroic.uppercase.domain.*;
+import com.morethanheroic.uppercase.domain.model.TimeObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZoneOffset;
@@ -14,6 +21,8 @@ import java.util.function.Function;
 @SpringBootApplication
 public class UpperFunctionApplication {
 
+    Logger logger = LoggerFactory.getLogger(UpperFunctionApplication.class);
+
     public static void main(String[] args) throws Exception {
         SpringApplication.run(UpperFunctionApplication.class, args);
     }
@@ -22,9 +31,34 @@ public class UpperFunctionApplication {
     @Bean
     public Function<HealthFirstMemberRequest, HealthFirstMemberResponse> members() {
         return member -> {
+
+            String currentTime = "unavailable";
+            try {
+                String url = "jdbc:mysql://35.187.39.130:3306/carparts";
+                String username = "root";
+                String password = "hidden";
+
+                Connection conn = DriverManager.getConnection(url, username, password);
+                Statement stmt = conn.createStatement();
+                ResultSet resultSet = stmt.executeQuery("SELECT NOW()");
+
+                if (resultSet.next()) {
+                    currentTime = resultSet.getObject(1).toString();
+                }
+
+                logger.debug("Successfully executed query.  Result: " + currentTime);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                logger.debug("Caught exception: " + e.getMessage());
+            }
+
+
+
             HealthFirstMemberResponse response = new HealthFirstMemberResponse();
-            response.setMemberId(member.getMemberId());
+            response.setMemberId(currentTime);
             response.setCoverage(HealthFirstMemberResponse.Coverage.MEDICAL);
+
             return response;
         };
     }
