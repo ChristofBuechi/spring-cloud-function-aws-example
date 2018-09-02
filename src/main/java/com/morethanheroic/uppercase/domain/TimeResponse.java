cf376d710c5d23e30b5d5f55dc8ed82b2fe5f1ff
Christof Buechi <christof.buechi@gmail.com>
2018-09-02 11:28:35 +0200

    add api-gateway compatible response object

--- src/main/java/com/morethanheroic/uppercase/domain/TimeResponse.java
+++ src/main/java/com/morethanheroic/uppercase/domain/TimeResponse.java
@@ -1,13 +1,44 @@
 package com.morethanheroic.uppercase.domain;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TimeResponse {
-    private String time;
 
-    public String getTime() {
-        return time;
+    public boolean isBase64Encoded;
+    public int statusCode;
+    public Map<String,String> headers;
+    public String body;
+
+    public TimeResponse() {
+        isBase64Encoded=false;
+        statusCode=201;
+        headers = new HashMap<>();
+        body = "1234";
     }
 
-    public void setTime(String time) {
-        this.time = time;
+
+    public int getStatusCode() {
+        return statusCode;
     }
+
+    public void setStatusCode(int statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public Map<String, String> getHeaders() {
+        return headers;
+    }
+
+    public void setHeaders(Map<String, String> headers) {
+        this.headers = headers;
+    }
+
+        public void setTime(String body) {
+            this.body = body;
+        }
+
+        public String getBody() {
+            return body;
+        }
 }
